bed6c0c48877d13e8969467f0cba4363d6cf68b0
cargo9 <168007770+cargo9@users.noreply.github.com>
2025-02-19 19:09:25 +0200

    .

---
+++ calculator.js
@@ -0,0 +1,19 @@
+const x = Number(prompt("Введіть перше число для додавання:"));
+const y = Number(prompt("Введіть друге число для додавання:"));
+
+alert(x + " + " + y + " = " + (x + y));
+
+const e = Number(prompt("Введіть перше число для віднімання:"));
+const r = Number(prompt("Введіть друге число для віднімання:"));
+
+alert(e + " - " + r + " = " + (e - r));
+
+const t = Number(prompt("Введіть перше число для множення:"));
+const u = Number(prompt("Введіть друге число для множення:"));
+
+alert(t + " * " + u + " = " + (t * u));
+
+const i = Number(prompt("Введіть перше число для ділення:"));
+const o = Number(prompt("Введіть друге число для ділення:"));
+
+alert(i + " / " + o + " = " + (i / o));
